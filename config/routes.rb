5c987f45f6e622897d8d855184f3ba65c712961a
Joe Szafarowicz <joeszaf+github@gmail.com>
2019-06-26 15:51:01 -0400

    update landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
   root 'homes#index'
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }, skip: [:sessions, :registrations, :passwords]
+  devise_for :users
 
-  devise_scope :user do
-    get 'sign_in', to: "devise/sessions#new", as: :new_user_session
-    delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
-  end
+  # devise and omniauth
+  # devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }, skip: [:sessions, :registrations, :passwords]
 
   resources :spaces, only: [:index, :show, :new]
   resources :events, only: [:index, :show, :new]
   resources :homes, only: [:index]
+  resources :users, only: [:show]
 
   namespace :api do
     namespace :v1 do
